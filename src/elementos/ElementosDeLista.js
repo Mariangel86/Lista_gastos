afc5fe0fe375b81a7fa139b9550b4d581bdd6353
Mariangel86 <Mariangel_antequera@hotmail.com>
2022-11-17 23:24:08 -0500

    changes of thursd

--- src/elementos/ElementosDeLista.js
+++ src/elementos/ElementosDeLista.js
@@ -70,9 +70,6 @@ const Categoria = styled.div`
     @media (max-width: 50rem) { /* 800px */
         font-size: 1.12rem;
     }
-    @media (min-width: 10rem) { /* 160px */
-        font-size: 1.12rem;
-    }
 `;
  
 const Descripcion = styled.div`
@@ -80,9 +77,6 @@ const Descripcion = styled.div`
     font-size: 1.25rem;
     text-transform: capitalize;
     @media (max-width: 50rem) { /* 50px */
-        justify-content: end;
-    }
-    @media (min-width: 10rem) { /* 50px */
         justify-content: end;
     }
 `;
